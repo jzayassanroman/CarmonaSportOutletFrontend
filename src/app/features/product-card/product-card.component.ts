fdb5bea1303e4b46d43848e431b763c67936ffc7
juaki <jcasquetsegura@safareyes.es>
2025-03-26 16:22:40 +0100

    tarjetas

--- src/app/features/product-card/product-card.component.ts
+++ src/app/features/product-card/product-card.component.ts
@@ -11,10 +11,43 @@ import {HttpClientModule} from '@angular/common/http';
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css'],
 })
+
 export class ProductCardComponent {
+
+
+  ngOnInit() {
+    this.products.forEach(product => {
+      product.currentImageIndex = 0;
+    });
+  }
   products = [
-    { name: 'Cinta de correr', price: 350, image: 'assets/treadmill.jpg' },
-    { name: 'Nike Zoom', price: 100, image: 'assets/nike-zoom.jpg' },
-    { name: 'Pala de pádel', price: 350, image: 'assets/padel.jpg' },
-  ];
+    {
+      name: 'Nike Zoom',
+      price: 100,
+      images: ['url_imagen1.jpg', 'url_imagen2.jpg'],
+      currentImageIndex: 0
+    },
+    {
+      name: 'Adidas Superstar',
+      price: 80,
+      images: ['url_imagen3.jpg', 'url_imagen4.jpg'],
+      currentImageIndex: 0
+    },
+
+    {
+      name: 'Adidas ',
+      price: 15,
+      images: ['url_imagen5.jpg', 'url_imagen6.jpg'],
+      currentImageIndex: 0
+    }
+
+];
+
+  nextImage(product: any) {
+    product.currentImageIndex = (product.currentImageIndex + 1) % product.images.length;
+  }
+
+  prevImage(product: any) {
+    product.currentImageIndex = (product.currentImageIndex - 1 + product.images.length) % product.images.length;
+  }
 }
