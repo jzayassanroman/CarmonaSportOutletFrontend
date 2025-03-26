6133aa6c6d9ea8cc84ff66de36e97e5e04c49a65
juaki <jcasquetsegura@safareyes.es>
2025-03-26 16:50:56 +0100

    tarjetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import {NavbarComponent} from './features/navbar/navbar.component';
 import {ProductCardComponent} from './features/product-card/product-card.component';
-import {NavbarComponent} from './navbar/navbar.component';
 import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ProductCardComponent} from '../features/product-card/product-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductCardComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
