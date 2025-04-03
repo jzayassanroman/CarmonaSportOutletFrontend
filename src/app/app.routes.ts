d1da58a3c7ba9c73c3a1ba004a831cabd0c52a9e
jzayassanroman <jzayassanroman@gmail.com>
2025-04-03 10:48:34 +0200

    landing page terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,18 @@
 import { Routes } from '@angular/router';
-import {NavbarComponent} from './features/navbar/navbar.component';
 import {ProductCardComponent} from './features/product-card/product-card.component';
-import {HomeComponent} from './home/home.component';
-import {NavbarComponent} from './navbar/navbar.component';
+import {HomeComponent} from './features/home/home.component';
 import {RegistroComponent} from './features/registro/registro.component';
 import {RegistroclienteComponent} from './features/registro-cliente/registrocliente/registrocliente.component';
 import {VerificacionComponent} from './features/verificacion/verificacion.component';
 import {LoginComponent} from './features/login/login.component';
+import {LandingComponent} from './features/landing/landing.component';
 
 export const routes: Routes = [
   { path: 'registeruser', component: RegistroComponent },
   { path: 'registerclient', component: RegistroclienteComponent },
   {path: 'verify', component: VerificacionComponent },
   {path: 'login', component: LoginComponent},
-  // { path: '', component: HomeComponent },
-  { path: 'productocard', component:ProductCardComponent },
-
-
-  {path: '', component: HomeComponent}
+  {path: 'productocard', component:ProductCardComponent },
+  {path: 'home', component: HomeComponent},
+  {path: '', component: LandingComponent},
 ];
